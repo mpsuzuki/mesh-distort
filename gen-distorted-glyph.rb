c4c9f83e779994295cfd308d0c7c8622a77d6e0c
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2025-07-01 17:39:04 +0900

    gen-distorted-glyph.rb: improve AA output

--- gen-distorted-glyph.rb
+++ gen-distorted-glyph.rb
@@ -42,11 +42,11 @@ class FT_Glyph_Metrics < FFI::Struct
 
     :horiBearingX, :long,
     :horiBearingY, :long,
-    :horiAdvance, :long,
+    :horiAdvance,  :long,
 
     :vertBearingX, :long,
     :vertBearingY, :long,
-    :vertAdvance, :long
+    :vertAdvance,  :long
   )
 end
 class FT_Bitmap < FFI::Struct
@@ -79,6 +79,25 @@ class FT_GlyphSlotRec < FFI::Struct
     :bitmap,      FT_Bitmap,
     :bitmap_left, :int,
     :bitmap_top,  :int
+  )
+end
+class FT_Size_Metrics < FFI::Struct
+  layout(
+    :x_ppem,       :ushort,
+    :y_ppem,       :ushort,
+    :x_scale,      :long,
+    :y_scale,      :long,
+    :ascender,     :long,
+    :descender,    :long,
+    :height,       :long,
+    :max_advance,  :long
+  )
+end
+class FT_SizeRec < FFI::Struct
+  layout(
+    :face, :pointer,
+    :generic, [:pointer, 2],
+    :metrics, FT_Size_Metrics
   )
 end
 class FT_FaceRec < FFI::Struct
@@ -114,7 +133,8 @@ class FT_FaceRec < FFI::Struct
     :underline_position,  :short,
     :underline_thickness, :short,
 
-    :glyph,        :pointer
+    :glyph,        :pointer,
+    :size,         :pointer
   )
 end
 
@@ -132,13 +152,20 @@ Opts = {
   "b" => 1,
   "c" => 14,
   "gid" => 0,
-  "ucs" => 0x41, # 'A'
+  "aa" => false,
+  "utf8" => "A",
+  "uhex" => nil,
   "seed" => 0xDEADBEEF,
   "mesh" => 1,
   "width" => 0,
   "height" => 32
 }
 require "getOpts.rb"
+if (Opts["uhex"] != nil)
+  Opts["uhex"] = Opts.uhex.gsub(/^[Uu]\+/, "").hex()
+elsif (Opts["utf8"] != nil)
+  Opts["uhex"] = Opts["utf8"].split("").first.encode("ucs-4be").unpack("N").first
+end
 
 # === INITIALIZE FREETYPE ===
 ft_lib_ptr = FFI::MemoryPointer.new(:pointer)
@@ -151,34 +178,33 @@ ft_face_ptr = FFI::MemoryPointer.new(:pointer)
 ft_err = FreeType::C.FT_New_Face(ft_lib, Opts.font, 0, ft_face_ptr)
 raise "FT_New_Face() failed" unless ft_err == 0
 ft_face = FT_FaceRec.new(ft_face_ptr.read_pointer())
-
-p ft_face[:num_faces]
-p ft_face[:face_index]
+p ft_face_ptr.read_pointer()
 
 # === SET PIXEL SIZE ===
 ft_err = FreeType::C.FT_Set_Pixel_Sizes(ft_face, Opts.width, Opts.height)
 raise "FT_Set_Pixel_Sizes() failed" unless ft_err == 0
 
+ft_size = FT_SizeRec.new(ft_face[:size])
+p ft_size[:face]
+
+ft_metric = ft_size[:metrics]
+printf("x_ppem=0x%x\n", ft_metric[:x_ppem])
+printf("y_ppem=0x%x\n", ft_metric[:y_ppem])
+printf("ascender=0x%016x\n", ft_metric[:ascender])
+printf("descender=0x%016x\n", ft_metric[:descender])
+printf("height=0x%x\n", ft_metric[:height])
+printf("max_advance=0x%x\n", ft_metric[:max_advance])
+
 # === RENDER GLYPH ===
-if (Opts.gid == 0 && Opts.ucs != 0)
-  Opts.gid = FreeType::C.FT_Get_Char_Index(ft_face, Opts.ucs)
+if (Opts.gid == 0)
+  Opts.gid = FreeType::C.FT_Get_Char_Index(ft_face, Opts.uhex)
 end
 ft_err = FreeType::C.FT_Load_Glyph(ft_face, Opts.gid, FreeType::C::FT_LOAD_RENDER)
 raise "FT_Load_Glyph() failed" unless ft_err == 0
-p ft_face[:num_glyphs]
-p ft_face[:underline_position]
-p ft_face[:underline_thickness]
-p Opts.gid
 
 ft_glyphslot_ptr = ft_face[:glyph]
 ft_glyphslot = FT_GlyphSlotRec.new(ft_glyphslot_ptr)
 
-p ["original library", ft_lib]
-p ["library in glyph slot", ft_glyphslot[:library]]
-p ft_face_ptr.read_pointer()
-p ft_glyphslot[:face]
-
-p ft_glyphslot[:glyph_index]
 ft_err = FreeType::C.FT_Render_Glyph(ft_glyphslot_ptr, 0)
 raise "FT_Render_Glyph() failed" unless ft_err == 0
 ft_bitmap = ft_glyphslot[:bitmap]
@@ -186,7 +212,16 @@ ft_bitmap = ft_glyphslot[:bitmap]
 # === EXTRACT BITMAP DATA ===
 glyph_width  = ft_bitmap[:width]
 glyph_height = ft_bitmap[:rows]
-p [glyph_width, glyph_height]
+glyph_advance_to_baseline  = ft_glyphslot[:bitmap_top]
+glyph_left_bearing = ft_glyphslot[:bitmap_left]
+
+p ["Opts.width:", Opts.width]
+p ["Opts.height:", Opts.height]
+p ["bitmap_width:", glyph_width]
+p ["bitmap_height:", glyph_height]
+p ["advance_to_baseline:", glyph_advance_to_baseline]
+p ["bearing_left:", glyph_left_bearing]
+
 
 ft_pixel_buffer_ptr = ft_bitmap[:buffer]
 nbytes_row = ft_bitmap[:pitch]
@@ -195,20 +230,41 @@ nbytes_buffer = nbytes_row * glyph_height
 arr_pixels = ft_pixel_buffer_ptr.read_bytes(nbytes_buffer).unpack("C*")
 
 ## === DUMP BITMAP ===
-#(0..glyph_height).to_a().each do |iy|
-#  line = Array.new()
-#  (0..glyph_width).to_a().each do |ix|
-#    pxl = arr_pixels[(iy * nbytes_row) + ix]
-#    if (pxl == nil)
-#      break
-#    elsif (pxl > 0)
-#      line << "#"
-#    else
-#      line << " "
-#    end
-#  end
-#  printf("%s\n", line.join(""))
-#end
+if (Opts.aa)
+  (0...(Opts.height - glyph_advance_to_baseline)).to_a().each do |iy|
+    puts(" " * (glyph_left_bearing + glyph_width))
+  end
+
+  (0...glyph_height).to_a().each do |iy|
+    line = [ " " * glyph_left_bearing ]
+    (0...glyph_width).to_a().each do |ix|
+      pxl = arr_pixels[(iy * nbytes_row) + ix]
+      if (pxl == nil)
+        break
+      elsif (pxl > 0)
+        line << "#"
+      else
+        line << " "
+      end
+    end
+    printf("%s\n", line.join(""))
+  end
+
+  (0...(glyph_advance_to_baseline - glyph_height)).to_a().each do |iy|
+    puts(" " * (glyph_left_bearing + glyph_width))
+  end
+
+
+  baseline_y = 0
+  bitmap_bottom = ft_glyphslot[:bitmap_top] - ft_bitmap[:rows]
+  descender_px = ft_metric[:descender] >> 6
+
+  space_under_baseline = bitmap_bottom - descender_px
+  (0...space_under_baseline).to_a().each do |iy|
+    puts(" " * (glyph_left_bearing + glyph_width))
+  end
+
+end
 
 # === CREATE IMAGE ===
 require 'rmagick'
@@ -229,22 +285,13 @@ points = []
     dy = ((xorshift32.next() & 0x1F) - 0xF) * Opts.strength / 0xF
     dst_x = src_x + dx
     dst_y = src_y + dy
-    p [dx, dy]
     points << src_x
     points << src_y
     points << dst_x
     points << dst_y
   end
 end
-p points
-#magick_image_distorted = magick_image.distort(Magick::BilinearDistortion, points, true)
 magick_image_distorted = magick_image.distort(Magick::ShepardsDistortion, points, true)
-#magick_image_distorted = magick_image.distort(Magick::BilinearDistortion, [
-#  0.0, 0.0,   10.0, 10.0,   # top-left
-#  99.0, 0.0,  99.0, 0.0,    # top-right
-#  99.0, 99.0, 99.0, 99.0,   # bottom-right
-#  0.0, 99.0,  0.0, 99.0     # bottom-left
-#], true)
 
 # === SAVE IMAGE ===
 mask_orig = magick_image.transparent("white")
